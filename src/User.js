becfbfbf901ffef99bab620d171d99f94349c948
Akhil Siraswal <akhilsiraswal@gmail.com>
2020-11-08 17:44:01 +0530

    08-11-20

--- src/User.js
+++ src/User.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function App() {
+export default function User() {
   return (
     <div>
-      <h1>App component</h1>
+      <h1>User component</h1>
     </div>
   );
 }
